77f769dbc54691dc066f05f7b930a5f52abd6a79
sujay1816 <65916624+sujay1816@users.noreply.github.com>
2026-05-04 16:40:40 +0530

    Update route.ts

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -9,7 +9,7 @@ export async function GET(request: Request) {
   if (code) {
     const cookieStore = cookies()
     const supabase = createServerClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!, {
-      cookies: { getAll() { return cookieStore.getAll() }, setAll(s) { s.forEach(({ name, value, options }) => cookieStore.set(name, value, options as any)) } }
+     cookies: { getAll() { return cookieStore.getAll() }, setAll(s: { name: string; value: string; options: any }[]) { s.forEach(({ name, value, options }) => cookieStore.set(name, value, options)) 
     })
     await supabase.auth.exchangeCodeForSession(code)
   }
