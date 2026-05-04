4bc5a109b092f2595e5515f79364beca62d1cfbe
sujay1816 <65916624+sujay1816@users.noreply.github.com>
2026-05-04 16:43:43 +0530

    Update route.ts

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -8,9 +8,22 @@ export async function GET(request: Request) {
   const redirect = url.searchParams.get('redirect') || '/'
   if (code) {
     const cookieStore = cookies()
-    const supabase = createServerClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!, {
-     cookies: { getAll() { return cookieStore.getAll() }, setAll(s: { name: string; value: string; options: any }[]) { s.forEach(({ name, value, options }) => cookieStore.set(name, value, options)) 
-    })
+    const supabase = createServerClient(
+      process.env.NEXT_PUBLIC_SUPABASE_URL!,
+      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+      {
+        cookies: {
+          getAll() {
+            return cookieStore.getAll()
+          },
+          setAll(cookiesToSet: { name: string; value: string; options: any }[]) {
+            cookiesToSet.forEach(({ name, value, options }) =>
+              cookieStore.set(name, value, options)
+            )
+          },
+        },
+      }
+    )
     await supabase.auth.exchangeCodeForSession(code)
   }
   return NextResponse.redirect(new URL(redirect, request.url))
